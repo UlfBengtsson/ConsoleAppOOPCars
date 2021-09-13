db4eedcb8f2baa25f546ed356446db7f2c62c7cf
Ulf Bengtsson <Ulf.Bengtsson@Lexicon.se>
2021-09-13 11:54:03 +0200

    Add project files.

---
+++ ConsoleAppOOPCars/Car.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleAppOOPCars
+{
+    class Car
+    {
+    //All members (states or behaviors will have by default the access modefier of privet)
+
+    //States
+        //Fields
+        string engine;//default value is null for string
+        bool engineIsOn;//default value is false for bool
+        string modelName;
+        int modelYear;//default value is 0 for int
+        string color;
+
+        //Property
+        public string Color
+        {
+            get
+            {
+                return color;
+            }
+            set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentException("Color can´t be Null or empty.");
+                }
+                else
+                {
+                    color = value;
+                }
+            }
+        }
+
+    //Behavior
+
+        //Constructor(s)
+            //if there is no constructor, c# will make a Zero constructor and use it.
+        public Car(string modelName, string color)
+        {
+            this.modelName = modelName;
+            Color = color;
+        }
+
+        public Car(string modelName, string color, int modelYear, string engineType)
+        {
+            this.modelName = modelName;
+            this.Color = color;
+            this.modelYear = modelYear;
+            this.engine = engineType;
+        }
+
+        //Methods
+        public string Drive()
+        {
+            if (engineIsOn)//(engineIsOn == true)
+            {
+                return $"The {color} {modelName} drives away.";
+            }
+            else
+            {
+                return "Can´t drive the car when the engine is off.";
+            }
+        }
+
+        public string StartEngine()
+        {
+            if (engineIsOn)
+            {
+                return "Engine is already on.";
+            }
+            else
+            {
+                engineIsOn = true;
+                return $"The {(engine == null ? "engine" : engine)} starts";
+            }
+        }
+
+        public string Info()
+        {
+            return $"-- Car Info --\nModel: {modelName}\nColor: {color}\nYear: {modelYear}\nEngine: {engine}\n-----\n";
+        }
+    }
+}
---
+++ ConsoleAppOOPCars/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ConsoleAppOOPCars
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Car myCar = new Car("525i","Blue", 2001, "V6");
+            //myCar.Color = "Blue";
+
+            //Console.WriteLine("Car color is: " + myCar.Color);
+
+            Car dadsCar = new Car("C3","Silver");
+            //dadsCar.Color = "Silver";
+
+            List<Car> cars = new List<Car>();
+
+            cars.Add(dadsCar);
+            cars.Add(myCar);
+
+            foreach (Car aCar in cars)
+            {
+                Console.WriteLine(aCar.Info());
+            }
+
+            Console.WriteLine(myCar.StartEngine());
+            Console.WriteLine(dadsCar.StartEngine());
+
+            foreach (Car aCar in cars)
+            {
+                Console.WriteLine(aCar.Drive());
+            }
+        }
+    }
+}
